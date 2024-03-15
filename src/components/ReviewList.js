b097480972ff24c4ab5526685b74fbc1b3704cf9
0813turara@naver.com <0813turara@naver.com>
2024-03-16 03:56:24 +0900

    Create component corresponding to one element

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,11 +1,33 @@
-// ReviewList 컴포넌트에서는 items 배열을 prop으로 받는다
+import './ReviewList.css';
+
+// 숫자형인 createAt 값을 예쁘게 보여주기 위해서, formatDate 함수 생성
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
+// 요소 하나에 해당하는 ReviewListItem 컴포넌트
+function ReviewListItem({ item }) {
+  return (
+    <div className="ReviewListItem">
+      <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title}></img>
+      <div>
+        <h1>{item.title}</h1>
+        <h1>{item.rating}</h1>
+        <h1>{formatDate(item.createdAt)}</h1>
+        <h1>{item.content}</h1>
+      </div>
+    </div>
+  );
+}
+
 function ReviewList({ items }) {
-  // map 메소드로 각 요소를 렌더링한다
-  // map 메소드 안에서 JSX를 리턴하면, 마치 JSX 여러 개를 추가한 것처럼 동작한다
   return (
     <ul>
       {items.map((item) => {
-        return <li>{item.title}</li>
+        // ReviewListItem 컴포넌트를 추가한다
+        // map의 콜백 함수에서 JSX로 작성한 값을 리턴해준다
+        return <li><ReviewListItem item={item} /></li>
       })}
     </ul>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Movie Review</h1>
+  <App />
 );
