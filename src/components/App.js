6e0457f579190e6e9d3e0217821e4706d16b0761
0813turara@naver.com <0813turara@naver.com>
2024-03-18 23:56:07 +0900

    Delete function using filter function

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,31 @@
 import ReviewList from "./ReviewList";
-import items from '../mock.json';
+import mockItems from '../mock.json';    // mock.json에서 불러온 배열의 이름을 mockItems로 변경
 import { useState } from "react";
 
 function App () {
-  // order State 생성 - 정렬 기준을 선택할 수 있도록
   const [order, setOrder] = useState('createdAt');
 
-  // ReviewList 컴포넌트의 items prop으로 sortedItems를 넘겨준다
-  // sort 매소드를 사용해 rating이 높은 순으로 정렬하는데,
-  // order State의 값이 createdAt일 떄는 최신순으로, rating일 때는 베스트순으로 정렬한다
+  const [items, setItems] = useState(mockItems);
+
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
-  // 핸들러 함수 생성 - 사용자가 정렬 기준(State 값)을 선택할 수 있도록
   const handleNewestClick = () => setOrder('createdAt');
   const handleBestClick = () => setOrder('rating');
 
-  // 각 버튼에 onClick 이벤트 핸들러를 사용해, 각각의 핸들러 함수를 prop으로 내려준다
+  // id 를 파라미터로 받아와서 해당 id를 가진 요소를 items.filter로 걸러낸다
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id != id);
+    setItems(nextItems);
+  }
+
+  // handleDelete 함수를 ReviewList 컴포넌트의 onDelete prop으로 내려준다
   return (
     <div>
       <div>
         <button onClick={handleNewestClick}>최신순</button>
         <button onClick={handleBestClick}>베스트순</button>
       </div>
-      <ReviewList items={sortedItems} />
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,13 +1,18 @@
 import './ReviewList.css';
 
-// 숫자형인 createAt 값을 예쁘게 보여주기 위해서, formatDate 함수 생성
 function formatDate(value) {
   const date = new Date(value);
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
 }
 
-// 요소 하나에 해당하는 ReviewListItem 컴포넌트
-function ReviewListItem({ item }) {
+// onDelete prop을 추가하고
+// handleDeleteClick 함수를 생성해, onDelete 함수로 item의 id 값을 넘겨준다
+function ReviewListItem({ item, onDelete }) {
+  const handleDeleteClick = () => {
+    onDelete(item.id);
+  }
+
+  // onClick 핸들러로 handleDeleteClick을 내려준다
   return (
     <div className="ReviewListItem">
       <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title}></img>
@@ -16,18 +21,23 @@ function ReviewListItem({ item }) {
         <h1>{item.rating}</h1>
         <h1>{formatDate(item.createdAt)}</h1>
         <h1>{item.content}</h1>
+        <button onClick={handleDeleteClick}>삭제</button>
       </div>
     </div>
   );
 }
 
-function ReviewList({ items }) {
+// onDelete prop을 추가하고
+// ReviewListItem 컴포넌트에 그대로 onDelete prop을 내려준다
+function ReviewList({ items, onDelete }) {
   return (
     <ul>
       {items.map((item) => {
-        // ReviewListItem 컴포넌트를 추가한다
-        // map의 콜백 함수에서 JSX로 작성한 값을 리턴해준다
-        return <li><ReviewListItem item={item} /></li>
+        return (
+        <li>
+          <ReviewListItem item={item} onDelete={onDelete} />
+        </li>
+        );
       })}
     </ul>
   );
